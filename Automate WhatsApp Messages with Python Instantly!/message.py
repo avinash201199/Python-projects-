b832f6a9698ab2643b5259394eb3e75d4614c1d5
Avinash Singh <61057666+avinash201199@users.noreply.github.com>
2022-01-02 22:38:03 +0530

    Create message.py

---
+++ Automate WhatsApp Messages with Python Instantly!/message.py
@@ -0,0 +1,11 @@
+#import the downloaded library in python
+import pywhatkit
+#pywhatkit.sendwhatmsg():Runs a function from the imported library
+pywhatkit.sendwhatmsg("+9185000000", "avinash",23,5)
+
+#syntax: pywhatkit.sendwhatmsg("reciver no. with country code",
+# message, hours, minutes)
+
+
+
+
